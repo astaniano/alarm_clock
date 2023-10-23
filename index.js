7d4fe4a7bd591fffb5ea1b409d0e027aae6d7029
astaniano <debryniuk@gmail.com>
2023-10-24 01:54:11 +0300

    improvements on music

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 const time = document.getElementById('time')
 const info = document.getElementById('info')
-const startBtn = document.getElementById('start')
+// const startBtn = document.getElementById('start')
 const stopBtn = document.getElementById('stop')
+const audio = document.getElementById('audio')
 
-let oscillator;
+// startBtn.addEventListener('click', () => {
+//     info.innerText = `waiting for the specified time`
+// })
+
 let intervalCount = 0;
-
-startBtn.addEventListener('click', () => {
-    const context = new AudioContext()
-
-    oscillator = context.createOscillator()
-    oscillator.type = "sine"
-    oscillator.frequency.value = 800
-    oscillator.connect(context.destination)
-
-    info.innerText = `waiting for the specified time`
-})
-
-let isOscillatorStarted = false
 
 setInterval(() => {
     const currentHourAndMins = new Date().toString().slice(16, 21)
     info.innerText = `waiting for the specified time (interval count: ${intervalCount})`
 
-    if (currentHourAndMins === time.value && !isOscillatorStarted && oscillator) {
-        oscillator.start()
-        isOscillatorStarted = true
+    if (currentHourAndMins === time.value) {
+        audio.play()
     }
 
     intervalCount++
 }, 59000)
 
 stopBtn.addEventListener('click', () => {
-    if (oscillator && isOscillatorStarted) {
-        oscillator.stop()
-        info.innerText = 'stopped'
-    }
+    audio.pause()
 })
